82e2148c60985a7fae212dcf986d9d6a8195435e
Maarten <science@lightcurvefilms.com>
2024-06-12 23:19:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -29,25 +29,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc',
-]
 
-autodoc_default_options = {
-    'member-order': 'bysource',
-}
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-math_eqref_format = 'Eq.{number}'
 
 
 # COMMENTAAR MAARTEN: met onderstaande 'boolean' variabelen kan je de automatische nummering van
@@ -58,8 +40,8 @@ math_eqref_format = 'Eq.{number}'
 # In een Python script, wat dit bestand 'conf.py' is (daarom heeft het ook de uitgang .py), moet commentaar worden voorafgegaan door een #-teken. 
 #  Dat is dus anders dan in een .rst bestand!
 
-numfig = False
-numtab = False
+# numfig = False
+# numtab = False
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -67,7 +49,7 @@ numtab = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {
 #     "stickysidebar": "true",
 #     "description": "The reference documentation for all EMC-EFO software."
@@ -76,11 +58,11 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # I added this based on the entry of 4 August 2022 in https://github.com/readthedocs/sphinx_rtd_theme/issues/301
 # The issue was that the sphinx_rtd_theme has a bug of placing the equation numbers above te equations, and not to the right of them.
 # This seems to solve it, or rather, it is a work-around.
-html_css_files = ['css/custom.css']
+# html_css_files = ['css/custom.css']
 
 html_last_updated_fmt = '%b %d, %Y, %X'
