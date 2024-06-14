d1a8685c692731364c26570c4235ed8e75d2a226
Maarten <science@lightcurvefilms.com>
2024-06-14 10:50:59 +0200

    trying to get this working

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.append ( os.path.abspath ('../HandyTools') )
 sys.path.append ( os.path.abspath ('../DataTools') )
+sys.path.append ( os.path.abspath ('../PYtoCPP/DataTools') )
 
 from HandyTools import HandyTools
 
